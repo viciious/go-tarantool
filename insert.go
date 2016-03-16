82c987a95da73eb0beb4728512d5e23b7ae4928e
lomik <r.lomonosov@gmail.com>
2016-03-17 00:19:12 +0300

    insert

---
+++ insert.go
@@ -0,0 +1,41 @@
+package tnt
+
+import (
+	"bytes"
+
+	"gopkg.in/vmihailenco/msgpack.v2"
+)
+
+type Insert struct {
+	Space interface{}
+	Tuple []interface{}
+}
+
+var _ Query = (*Insert)(nil)
+
+func (s *Insert) Pack(requestID uint32, data *packData) ([]byte, error) {
+	var bodyBuffer bytes.Buffer
+	var err error
+
+	encoder := msgpack.NewEncoder(&bodyBuffer)
+
+	encoder.EncodeMapLen(2) // Space, Tuple
+
+	// Space
+	if err = data.writeSpace(s.Space, bodyBuffer, encoder); err != nil {
+		return nil, err
+	}
+
+	// Tuple
+	if s.Tuple != nil {
+		encoder.EncodeUint32(KeyTuple)
+		encoder.EncodeArrayLen(len(s.Tuple))
+		for _, value := range s.Tuple {
+			if err = encoder.Encode(value); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return packIproto(InsertRequest, requestID, bodyBuffer.Bytes()), nil
+}
---
+++ insert_test.go
@@ -0,0 +1,80 @@
+package tnt
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInsert(t *testing.T) {
+	assert := assert.New(t)
+
+	tarantoolConfig := `
+    s = box.schema.space.create('tester', {id = 42})
+    s:create_index('tester_id', {
+        type = 'hash',
+        parts = {1, 'NUM'}
+    })
+	s:create_index('tester_name', {
+        type = 'hash',
+        parts = {2, 'STR'}
+    })
+	s:create_index('id_name', {
+        type = 'hash',
+        parts = {1, 'NUM', 2, 'STR'},
+        unique = true
+    })
+    t = s:insert({1, 'First record'})
+    t = s:insert({2, 'Music'})
+    t = s:insert({3, 'Length', 93})
+
+    box.schema.user.create('writer', {password = 'writer'})
+	box.schema.user.grant('writer', 'write', 'space', 'tester')
+    `
+
+	box, err := NewBox(tarantoolConfig, nil)
+	if !assert.NoError(err) {
+		return
+	}
+	defer box.Close()
+
+	conn, err := box.Connect(&Options{
+		User:     "writer",
+		Password: "writer",
+	})
+	assert.NoError(err)
+	assert.NotNil(conn)
+
+	defer conn.Close()
+
+	data, err := conn.Execute(&Insert{
+		Space: "tester",
+		Tuple: []interface{}{4, "Hello"},
+	})
+
+	if assert.NoError(err) {
+		assert.Equal([]interface{}{
+			[]interface{}{
+				uint32(4),
+				"Hello",
+			},
+		}, data)
+	}
+
+	data, err = conn.Execute(&Insert{
+		Space: "tester",
+		Tuple: []interface{}{4, "Hello"},
+	})
+
+	if assert.Error(err) {
+		assert.Contains(err.Error(), "Duplicate key exists")
+	}
+}
+
+func BenchmarkInsertPack(b *testing.B) {
+	d, _ := newPackData(42)
+
+	for i := 0; i < b.N; i += 1 {
+		(&Insert{Tuple: []interface{}{3, "Hello world"}}).Pack(0, d)
+	}
+}
