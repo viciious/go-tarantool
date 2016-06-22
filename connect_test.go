c29fd23b9fdd774708ab1c91dc4e5dfbb7cfc743
zimnukhov <zimnuhov@gmail.com>
2016-06-22 15:11:48 +0300

    test default space option

--- connect_test.go
+++ connect_test.go
@@ -25,3 +25,34 @@ func TestConnect(t *testing.T) {
 
 	assert.Contains(string(conn.Greeting.Version), "Tarantool")
 }
+
+func TestDefaultSpace(t *testing.T) {
+	assert := assert.New(t)
+	config := `
+	s = box.schema.space.create('tester', {id = 42})
+	s:create_index('tester_id', {
+		type = 'hash',
+		parts = {1, 'NUM'}
+	})
+	t = s:insert({1})
+	`
+	box, err := NewBox(config, nil)
+	assert.NoError(err)
+	defer box.Close()
+
+	conn, err := Connect(box.Addr(), &Options{
+		DefaultSpace: "tester",
+	})
+
+	assert.NoError(err)
+	defer conn.Close()
+
+	tuples, err := conn.Execute(&Select{
+		Key:   1,
+		Index: "tester_id",
+	})
+	assert.NoError(err)
+	assert.Equal([][]interface{}{
+		[]interface{}{uint64(1)},
+	}, tuples)
+}
