16a52e549ae1301dde6af989b634458d1da4e07b
Victor Luchits <vluchits@gmail.com>
2018-10-29 13:40:11 +0300

    Disable the reuse of requests in request pool

--- request_pool.go
+++ request_pool.go
@@ -2,15 +2,21 @@ package tarantool
 
 type cappedRequestPool struct {
 	queue chan *request
+	reuse bool
 }
 
 func newCappedRequestPool() *cappedRequestPool {
 	return &cappedRequestPool{
 		queue: make(chan *request, 1024),
+		reuse: false,
 	}
 }
 
 func (p *cappedRequestPool) Get() (r *request) {
+	if !p.reuse {
+		return &request{}
+	}
+
 	select {
 	case r = <-p.queue:
 		r.opaque = nil
@@ -22,6 +28,10 @@ func (p *cappedRequestPool) Get() (r *request) {
 }
 
 func (p *cappedRequestPool) Put(r *request) {
+	if !p.reuse {
+		return
+	}
+
 	select {
 	case p.queue <- r:
 	default:
