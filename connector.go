92746a8e73f8c8775a1263e707bb9e703fa66b71
Alexandr Mayorskiy <a.mayorskiy@corp.mail.ru>
2018-12-07 15:20:15 +0300

    add context to Connector.Connect function

--- connector.go
+++ connector.go
@@ -2,31 +2,50 @@ package tarantool
 
 import (
 	"net/url"
-	"sync"
+	"context"
 )
 
 type Connector struct {
-	sync.Mutex
 	RemoteAddr string
 	options    Options
 	conn       *Connection
+	ch         chan struct{}
 }
 
 // New Connector instance.
 func New(dsnString string, options *Options) *Connector {
+	ch := make(chan struct{}, 1)
+	ch <- struct{}{}
+
 	if options != nil {
-		return &Connector{RemoteAddr: dsnString, options: *options}
+		return &Connector{RemoteAddr: dsnString, options: *options, ch: ch}
 	}
-	return &Connector{RemoteAddr: dsnString}
+	return &Connector{RemoteAddr: dsnString, ch: ch}
 }
 
 // Connect returns existing connection or will establish another one.
 func (c *Connector) Connect() (conn *Connection, err error) {
-	c.Lock()
+	return c.ConnectContext(context.Background())
+}
+
+// Connect returns existing connection or will establish another one.
+func (c *Connector) ConnectContext(ctx context.Context) (conn *Connection, err error) {
+	doneChan := ctx.Done()
+	if doneChan != nil {
+		select {
+		case <-doneChan:
+			return nil, ctx.Err()
+		case <-c.ch:
+		}
+	} else {
+		<- c.ch
+	}
+
 	if c.conn == nil || c.conn.IsClosed() {
 		var dsn *url.URL
 		dsn, c.options, err = parseOptions(c.RemoteAddr, c.options)
 		if err != nil {
+			c.ch <- struct {}{}
 			return nil, err
 		}
 		// clear possible user:pass in order to log c.RemoteAddr securely
@@ -34,17 +53,17 @@ func (c *Connector) Connect() (conn *Connection, err error) {
 		c.conn, err = connect(dsn.Scheme, dsn.Host, c.options)
 	}
 	conn = c.conn
-	c.Unlock()
+	c.ch <- struct {}{}
 
 	return conn, err
 }
 
 // Close underlying connection.
 func (c *Connector) Close() {
-	c.Lock()
-	defer c.Unlock()
+	<- c.ch
 	if c.conn != nil && !c.conn.IsClosed() {
 		c.conn.Close()
 	}
 	c.conn = nil
+	c.ch <- struct {}{}
 }
